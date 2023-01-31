ca1265b77ed747df175a526b850c8000f2aac82a
plima94 <pauloalima1994@gmail.com>
2023-01-31 14:55:09 +0000

    created first paddle

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 # Setup screen
 screen = Screen()
-screen.setup(800, 600)
 screen.bgcolor("black")
+screen.setup(800, 600)
+screen.title("Pong Game")
+screen.tracer(0)
 
+
+
+paddle = Turtle()
+screen.update()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(5, 1)
+paddle.penup()
+paddle.setposition(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,2 +1,12 @@
 from turtle import Turtle
 
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+
+class Paddle:
+    def __init__(self):
+        self.segments = []
+
+
+
+
